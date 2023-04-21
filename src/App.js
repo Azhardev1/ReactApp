a35153766a4efe26ce80ccd5ec82736825ef0afa
Azhar S <azhar.dev26@gmail.com>
2023-04-21 18:04:24 +0530

    react App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Application from './components/Application';
 
 function App() {
+  const expenses = [{
+    id: 'z1',
+    title: "z"
+    },
+    {
+    id: 'z2',
+    title: "abc"
+    }
+];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Application title={expenses[0].title}></Application>
     </div>
   );
 }
